6e21a5296043a9e322f593f246534960c64948ed
arristigit <arristimedia@gmail.com>
2021-11-21 23:45:05 +0530

    Static files settings updated

--- atg/settings.py
+++ atg/settings.py
@@ -143,9 +143,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = "/static/"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR / "static"),
 ]
 
 MEDIA_URL = '/media/'
